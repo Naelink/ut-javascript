b77055474e8410da6f572eefe93e534d62c50d62
Nael Laptop Acemagic <naelink@gmail.com>
2024-04-12 10:00:28 +0200

    cors

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const path = require('path');
 const app = express();
 const port = 3000; 
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
